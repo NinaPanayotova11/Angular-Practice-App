fcfe3eb63cf635a9873d09af3778a6f10b9738c2
NinaPanayotova11 <73670950+NinaPanayotova11@users.noreply.github.com>
2021-09-14 10:27:30 +0300

    Created a Recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,12 +18,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  recipes: Recipe[] = [
+    new Recipe(
+      'Cabbage with pulled pork',
+      'Description of Cabbage with pulled pork',
+      'https://www.supichka.com/files/images/514/fit_1400_933.jpg'
+    ),
+    new Recipe(
+      'Caesar salad ',
+      'Caesar salad with pork',
+      'https://littlesunnykitchen.com/wp-content/uploads/Caesar-Salad-10-1024x1536.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
